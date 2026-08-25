4e477629ef893895fb54c6ea0caeca9682a87960
RobertLib <rob@hotmail.cz>
2026-08-25 21:54:23 +0200

    Initial Commit

---
+++ chess/chessApp.swift
@@ -0,0 +1,17 @@
+//
+//  chessApp.swift
+//  chess
+//
+//  Created by Robert Libšanský on 25.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct chessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
